6b8be496c5fe7fd6c89e8293c6390fef4a47f1db
Raffy23 <raphael.ludwig@happywetter.at>
2017-09-20 15:16:00 +0200

    [Refactor] Password probing in Auto Discovery

--- jvm/src/main/scala/at/happywetter/boinc/util/BoincHostSettingsResolver.scala
+++ jvm/src/main/scala/at/happywetter/boinc/util/BoincHostSettingsResolver.scala
@@ -1,14 +1,15 @@
 package at.happywetter.boinc.util
 
-import java.net.{Inet4Address, InetAddress}
+import java.net.InetAddress
 import java.util.concurrent.ScheduledExecutorService
 
 import at.happywetter.boinc.AppConfig.Config
-import at.happywetter.boinc.{AppConfig, BoincManager}
 import at.happywetter.boinc.boincclient.BoincClient
+import at.happywetter.boinc.{AppConfig, BoincManager}
+import org.slf4j.LoggerFactory
 
-import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
 /**
   * Created by: 
@@ -18,38 +19,50 @@ import scala.concurrent.ExecutionContext.Implicits.global
   */
 class BoincHostSettingsResolver(config: Config, boincManager: BoincManager)(implicit val scheduler: ScheduledExecutorService) {
 
+  private val logger = LoggerFactory.getILoggerFactory.getLogger(this.getClass.getCanonicalName)
   private val autoDiscovery = new BoincDiscoveryService(config.autoDiscovery, discoveryCompleted)
 
   def beginSearch(): Unit =
     if (config.autoDiscovery.enabled)
       discoveryCompleted( autoDiscovery.search )
 
-  private def discoveryCompleted(data: Future[List[IP]]): Unit = data.foreach{ hosts =>
-    hosts.diff(getUsedIPs).foreach( ip => {
-      Future {
+  private def discoveryCompleted(data: Future[List[IP]]): Unit = {
+    data.foreach{ hosts =>
+      logger.debug("Completed Discovery: " + hosts)
 
-        config.autoDiscovery.password.map( password => {
-          val boincCoreClient = new BoincClient(ip.toString, config.autoDiscovery.port, password, config.boinc.encoding)
-          val succ = boincCoreClient.authenticate()
+      hosts.diff(getUsedIPs).foreach( ip => {
+        Future {
+          var found: Boolean = false
 
-          val result =
-            if (succ) (succ, boincCoreClient.getHostInfo.map(_.domainName), password)
-            else (succ, Future { "" }, "")
+          config.autoDiscovery.password.map( password => {
+            if (found)
+              (false, Future{""})
 
-          boincCoreClient.close()
-          result
-        }).find{ case (succ, _, _) => succ }
-          .foreach{ case (_, domainName, pw) =>
-            domainName.foreach( domainName =>
-              boincManager.add(
-                domainName,
-                AppConfig.Host(ip.toString, config.autoDiscovery.port.toShort, pw)
-              )
-            )
-          }
-      }
-    })
+            val boincCoreClient = new BoincClient(ip.toString, config.autoDiscovery.port, password, config.boinc.encoding)
+            found = boincCoreClient.authenticate()
+
+            val result =
+              if (found) (found, boincCoreClient.getHostInfo.map(_.domainName), password)
+              else (found, Future { "" }, "")
+
+            boincCoreClient.close()
+            result
+          }).find{ case (succ, _, _) => succ }
+            .foreach{ case (_, domainName, pw) =>
+              domainName.foreach( domainName => {
+                logger.debug(s"Found usable Core Client at $ip")
+
+                boincManager.add(
+                  domainName,
+                  AppConfig.Host(ip.toString, config.autoDiscovery.port.toShort, pw)
+                )
+              })
+            }
+        }
+      })
+    }
   }
+
 
   private def getUsedIPs: List[IP] = boincManager
     .getAddresses.filter(_._2 == config.autoDiscovery.port)
